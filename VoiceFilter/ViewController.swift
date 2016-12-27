00fac933bc7526254215c4d2ac9efafb004c4e38
Hector Montero <hamonteroa@Hectors-MacBook-Pro.local>
2016-12-27 11:00:38 -0500

    Initial Commit

---
+++ VoiceFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceFilter
+//
+//  Created by Hector Montero on 12/27/16.
+//  Copyright © 2016 Hector Montero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
